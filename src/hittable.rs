1197b805f37642ae4faf9a3c3cce09b6df5e4cf8
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2025-10-07 21:26:12 +0100

    implement Hittable trait for Sphere and InfinitePlane with intersection methods

---
+++ src/hittable.rs
@@ -0,0 +1,13 @@
+use crate::infinite_plane::InfinitePlane;
+use crate::material::Material;
+use crate::ray::{Ray, Segment};
+use crate::sphere::Sphere;
+use crate::vec3::Vec3;
+
+pub(crate) trait Hittable {
+    fn intersects_ray(&self, ray: &Ray) -> Option<(f32, Vec3, Vec3)>; // (t, point, normal)
+    fn intersects_segment(&self, segment: &Segment) -> Option<(f32, Vec3, Vec3)>;
+    fn material(&self) -> &Material;
+
+
+}
